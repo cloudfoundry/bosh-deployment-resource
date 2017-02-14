4efa98b0e4ef421fd7a87e195dd4adaef15da3ab
Dan Wendorf <dwendorf@pivotal.io>
2017-02-14 13:25:51 -0800

    Use local path for building packages
    [#137639773]
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -9,6 +9,6 @@ go get github.com/onsi/ginkgo/ginkgo
 
 cd gopath/src/github.com/cloudfoundry/bosh-deployment-resource
 
-go build -o "$BUILT_BINARIES_DIR/check" cmd/check
-go build -o "$BUILT_BINARIES_DIR/in" cmd/in
-go build -o "$BUILT_BINARIES_DIR/out" cmd/out
+go build -o "$BUILT_BINARIES_DIR/check" ./cmd/check
+go build -o "$BUILT_BINARIES_DIR/in" ./cmd/in
+go build -o "$BUILT_BINARIES_DIR/out" ./cmd/out
